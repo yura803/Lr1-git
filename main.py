0c2f63128846b4626ad99273eba81e3f07603f41
Yura <yura444715188@gmail.com>
2025-09-30 16:04:00 +0300

    Добавлена функция вычисления факториала

--- main.py
+++ main.py
@@ -19,3 +19,12 @@ def print_fibonacci_pyramid(rows):
 # Укажите количество строк пирамиды
 rows = 5
 print_fibonacci_pyramid(rows)
+
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+# Добавьте вызов функции в конец файла
+print(f"Факториал числа {rows}: {factorial(rows)}")
